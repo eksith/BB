080e16fbeaae1e2ce0b87a754c7dbe3e88880fbd
Eksith Rodrigo <eksith@live.com>
2014-12-04 05:20:38 -0500

    Removed session_id() usage and routing

--- functions.php
+++ functions.php
@@ -16,7 +16,8 @@ function formatBytes( $bytes, $precision = 2 ) {
 
 function formatTime( $time ) {
 	$time  = strtotime( $time );
-	return "<time datetime='". gmdate("Y-m-d H:i:s T", $time) ."'>" . gmdate("M d, Y H:i", $time) . "</time>";
+	return "<time datetime='". gmdate("Y-m-d H:i:s T", $time) ."'>" . 
+		gmdate("M d, Y H:i", $time) . "</time>";
 }
 
 function formatNav( $url, $text ) {
@@ -109,181 +110,6 @@ function printJS() {
 }
 
 /***
- Routing
- ***/
-
-function method() {
-	return strtolower( $_SERVER['REQUEST_METHOD'] );
-}
-
-function index( $page = 1 ) {
-	$m = method();
-	if ( 'get' == $m ) {
-		$action	= "/threads/{$page}";
-		$posts	= BB\Post::getPosts( 0, $page );
-		$root	= 0;
-		$parent	= 0;
-		require ( TEMPLATES . '_index.php' );
-		
-	} elseif ( 'post' == $m ) {
-		$title	= filter( 'post', 'title', null, 'raw' );
-		$body	= filter( 'post', 'body', null, 'raw' );
-		$post	= initPost( $title, $body, 0, 0, 0 );
-		
-		if ( $post->save( getAuth() ) ) {
-			header( 'Location: /posts/' . $post->id );
-			die();
-		} else {
-			die( 'error');
-		}
-	}
-}
-
-function firehose( $page = 1) {
-	if ( 'get' == $m ) {
-		$action	= "/threads/{$page}";
-		$posts	= BB\Post::getPosts( 0, $page, false, false, true );
-		$root	= 0;
-		$parent	= 0;
-		require ( TEMPLATES . '_index.php' );
-	}
-}
-
-function thread( $id = 0, $page = 1 ) {
-	$m = method();
-	if ( 'get' == $m ) {
-		$action	= "/threads/{$id}";
-		$posts = BB\Post::getPosts( $id, $page );
-		$root	= $id;
-		$parent	= 0;
-		require ( TEMPLATES . '_thread.php' );
-		
-	} elseif ( 'post' == $m ) {
-		$title	= filter( 'post', 'title', null, 'raw' );
-		$body	= filter( 'post', 'body', null, 'raw' );
-		$root	= filter( 'post', 'root', $id, 'num' );
-		$post	= initPost( $title, $body, $root, 0, 0 );
-		
-		if ( $post->save( getAuth() ) ) {
-			header( 'Location: /posts/' . $post->id );
-			die();
-		} else {
-			die( 'error');
-		}
-	}
-}
-// TODO: Moderation view
-function modView( $id ) {
-	
-}
-
-function flagView( $id ) {
-	$post = BB\Post::getInfo( $id );
-	if ( empty( $info ) ) {
-		die( 'No post found');
-	}
-}
-
-function editView( $id, $auth ) {
-	if ( empty( $auth ) ) {
-		die( 'Edit window expired' );
-	}
-	$posts		= BB\Post::getPosts( $id, 1, false, true );
-	
-	if ( empty( $posts ) ) {
-		die( 'No post found');
-	}
-	
-	$author		= matchAuth( $posts[0]->auth_key, $posts[0]->created_at );
-	$akey		= editKey( $id, $auth );
-	$canEdit	= ( $author && editKey( $id, $auth ) ) ? true : false;
-	
-	if ( !$canEdit ) {
-		delSessList( 'edits', $id );
-		die( 'Edit window expired' );
-	}
-	return $posts;
-}
-
-function view( $id, $act = 'read', $auth = '' ) {
-	$m = method();
-	if ( 'get' == $m ) {
-		$action	= "/posts/{$id}";
-		
-		switch( $act ) {
-			case 'read' :
-				$posts = BB\Post::getPosts( $id, 1, false );
-				$root	= $posts[0]->root_id;
-				$parent	= $id;
-				require ( TEMPLATES . '_thread.php' );
-				return;
-				
-			case 'lock':
-			case 'delete':
-				$posts = modView( $id );
-				break;
-				
-			case 'flag':
-				$posts = flagView( $id );
-				break;
-				
-			default :
-				$posts = editView( $id, $auth );
-				$root	= $posts[0]->root_id;
-				$parent	= $id;
-				require ( TEMPLATES . '_thread.php' );
-		}
-	} elseif ( 'post' == $m ) {
-		$body	= filter( 'post', 'body', null, 'raw' );
-		
-		if( empty( $body ) ) {
-			die( 'Post cannot be empty' );
-		}
-		
-		$title	= filter( 'post', 'title', null, 'title' );
-		$root	= filter( 'post', 'root', 0, 'num' );
-		$parent	= filter( 'post', 'parent', 0, 'num' );
-		$edit	= filter( 'post', 'edit', 0, 'num' );
-		
-		$post	= initPost( $title, $body, $root, $parent, $edit );
-		
-		if ( $post->save( getAuth() ) ) {
-			header( 'Location: /posts/' . $post->id );
-			die();
-		} else {
-			die( 'error');
-		}
-	}
-}
-
-function vote( $id, $ud ) {
-	if ( 'get' == method() ) {
-		$vote	= ( $ud == 'up' )? 1 : -1;
-		$info	= BB\Post::getInfo( $id );
-		
-		if ( empty( $info ) ) {
-			die( 'No post found' );
-		}
-		if ( matchAuth( $info['auth_key'], $info['created_at'] ) ) {
-			die( 'already voted' );
-		}
-		
-		putSessList( 'votes', $id, $vote );
-		die( 'voted' );
-	}
-	die( 'problem' );
-}
-
-function tag( $tag, $page = 1 ) {
-	$m = method();
-	if ( 'get' == $m ) {
-	} else {
-		return; // We only answer to get here
-	}
-}
-
-
-/***
  Helpers
  ***/
 
@@ -299,6 +125,16 @@ function initPost( $title, $body, $root, $parent, $id = 0 ) {
 	}
 	return $post;
 }
+
+function housekeeping() {
+	flush();
+	if ( function_exists( 'fastcgi_finish_request ' ) ) {
+		fastcgi_finish_request();
+	}
+	
+	//BB\Post::lockOld( AUTO_LOCK );
+}
+
 
 /***
  Security and privileges
@@ -465,19 +301,43 @@ function pbkdf2(
 	}
 }
 
-function editKey( $id, $auth = null ) {
-	if ( null === $auth ) {
-		$salt = bin2hex( mcrypt_create_iv( 5, MCRYPT_DEV_URANDOM ) );
-		return $salt . '.' . hash( 'tiger160,4', $id . $salt . session_id() );
+function shash( $salt, $key, $pbkdf = false ) {
+	if ( $pbkdf ) {
+		return $salt . '.' . pbkdf2( 'sha256', $key, $salt, 1000, 20 );
 	}
-	
-	$p = strpos( $auth, '.' );
+	return $salt . '.' . hash( 'tiger160,4', $key . $salt );
+}
+
+function salt( $size ) {
+	return bin2hex( mcrypt_create_iv( $size, MCRYPT_DEV_URANDOM ) );
+}
+
+function matchHash( $hash, $key, $pbkdf = false ) {
+	$p = strpos( $hash, '.' );
 	if  ( false === $p ) {
 		return false;
 	}
+	$salt = substr( $hash, 0, $p );
+	return $hash === shash( $salt, $key, $pbkdf );
+}
+
+function editKey( $id, $auth = null ) {
+	if ( null === $auth ) {
+		$salt = salt( 5 );
+		return shash( $salt, $id );
+	}
 	
-	$salt = substr( $auth, 0, $p );
-	return $auth === $salt . '.' . hash(  'tiger160,4', $id . $salt . session_id() );
+	return matchHash( $auth, $id );
+}
+
+function getAuth( $salt = null, $pass = null ) {
+	if ( null === $salt ) {
+		$salt = salt( 16 );
+	}
+	if ( null === $pass ) {
+		$pass = session_id();
+	}
+	return shash( $salt, $pass, true );
 }
 
 function matchAuth( $key, $created_at = null ) {
@@ -502,16 +362,6 @@ function matchAuth( $key, $created_at = null ) {
 	}
 	
 	return false;
-}
-
-function getAuth( $salt = null, $pass = null ) {
-	if ( null === $salt ) {
-		$salt = bin2hex( mcrypt_create_iv( 16, MCRYPT_DEV_URANDOM ) );
-	}
-	if ( null === $pass ) {
-		$pass = session_id();
-	}
-	return $salt . '.' . pbkdf2( 'sha256', session_id(), $salt, 1000, 20 );
 }
 
 function getEditLimit() {
@@ -553,9 +403,17 @@ function getSessList( $key ){
 	return $_SESSION[$key];
 }
 
+function requestKey( $reset = false ) {
+	if ( !isset( $_SESSION['req'] ) || $reset ) {
+		$_SESSION['req'] = bin2hex( mcrypt_create_iv( 5, MCRYPT_DEV_URANDOM ) );
+	}
+	if ( !$reset ) {
+		return $_SESSION['req'];
+	}
+}
 
 function field( $key ) {
-	return hash( 'tiger160,4', $key . session_id() );
+	return hash( 'tiger160,4', $key . requestKey() );
 }
 
 function fromArray( $array, $key, $default, $hash = false ) {
