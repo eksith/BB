8460a65f0cb3de0004f13012bdbe38166faaf024
Eksith Rodrigo <eksith@live.com>
2014-12-11 11:11:05 -0500

    Comments!

--- vendor/BB/Models/Data.php
+++ vendor/BB/Models/Data.php
@@ -2,20 +2,51 @@
 namespace BB\Models;
 
 abstract class Data implements \SplObserver {
+	
+	/**#@+
+	 * Common object properties
+	 */
+	
+	/**
+	 * @var Object id. Populated on creation or should be populated before editing
+	 */
 	public $id		= 0;
 	
+	/**
+	 * @var Created date and time. Set by the database
+	 */
 	public $created_at;
 	
+	/**
+	 * @var Last update date and time. Also set by the database
+	 */
 	public $updated_at;
 	
+	/**
+	 * @var Treatment status
+	 * @example -1 For pseudo delete
+	 */
 	public $status;
 	
+	/**
+	 * @var Any errors during editing, creating
+	 */
+	public $errors		= array();
+	
+	/**#@-*/
+	
+	/**
+	 * @var Data connection object (may be changed to connection array later)
+	 */
 	protected static $db = null;
 	
 	abstract public static function find( $filter = array() );
 	
 	abstract public function update( \SplSubject $subject );
 	
+	/**
+	 * Connection initiator
+	 */
 	public static function init() {
 		if ( is_object( self::$db ) ) {
 			return;
@@ -27,6 +58,9 @@ abstract class Data implements \SplObserver {
 		self::$db->setAttribute( \PDO::ATTR_DEFAULT_FETCH_MODE, \PDO::FETCH_ASSOC );
 	}
 	
+	/**
+	 * Execute a query with parameters and fetch by selector options
+	 */
 	protected static function find( $sql, $params, $fetch = 'class' ) {
 		$params = self::parametize( $params );
 		
@@ -62,6 +96,16 @@ abstract class Data implements \SplObserver {
 		}
 	}
 	
+	/**
+	 * TODO: Select query
+	 */
+	protected static select( $table, $params, $where ) {
+		
+	}
+	
+	/**
+	 * Single table update
+	 */
 	protected static function edit( $table, $params, $where ) {
 		$sql	= self::updateQuery( $table, $params, $where );
 		$params = self::parametize( $params );
@@ -71,6 +115,10 @@ abstract class Data implements \SplObserver {
 		return $stmt->execute( $params );
 	}
 	
+	/**
+	 * Single table insert
+	 * @return bool|int Insert if requested or execute success
+	 */
 	protected static function put( $table, $params, $response = 'id' ) {
 		$sql	= insertQuery( $table, $params );
 		$params = self::parametize( $params );
@@ -85,6 +133,10 @@ abstract class Data implements \SplObserver {
 		return $stmt->execute( $params );
 	}
 	
+	/**
+	 * Single table delete
+	 * @return bool Execute success
+	 */
 	protected static function delete( $table, $id, $permanant = false ) {
 		if ( $permanant ) {
 			$sql	= "DELETE FROM $table WHERE id = :i;";
@@ -95,6 +147,21 @@ abstract class Data implements \SplObserver {
 		self::init();
 		$stmt = self::$db->prepare( $sql );
 		return $stmt->execute( array( ':i' => $id ) );
+	}
+	
+	private static function selectQuery( $table, $fields, $where ) {
+		$sql	= 'SELECT ' . implode( ',', $fields ) . " FROM $table";
+		if ( empty( $where ) ) {
+			return $sql;
+		}
+		
+		$sql	.= ' WHERE';
+		$keys	= array_keys( $where );
+		
+		foreach( $keys as $v ) {
+			$sql .= " $v = :$v,";
+		}
+		return rtrim( $sql, ',' ) . ';';
 	}
 	
 	private static function updateQuery( $table, &$params, $where ) {
@@ -138,6 +205,10 @@ abstract class Data implements \SplObserver {
 		return rtrim( $sql, ',' ) . ' )';
 	}
 	
+	
+	/**
+	 * Converts an array of parameter=>values to :parameter=> values
+	 */
 	private static function parametize( $params ) {
 		$keys = array_map( 
 			function( $k ) {
