8cfc4941717c60b351342eca0edf70fe326ea560
Eksith Rodrigo <eksith@live.com>
2014-12-25 07:41:28 -0500

    Streamlined and fixed CSS path. Added search box

--- vendor/BB/templates/_index.php
+++ vendor/BB/templates/_index.php
@@ -4,11 +4,12 @@
 <meta http-equiv='Content-Type' content='text/html; charset=utf-8' />
 <meta name='viewport' content='width=device-width' />
 <title>Forum</title>
-<link rel='stylesheet' href='s.css' />
+<link rel='stylesheet' href='/assets/s.css' />
 <link rel='shortcut icon' type='image/png' href='assets/img/favicon.png' />
 </head>
 <body>
-<nav class='main'><ul><li class='active'><a href='.'><img src='assets/img/favicon.png' /> Home</a></li><li><a href='/firehose'>New</a></li><li><a href='/archive.html'>Archive</a></li></ul></nav>
+<nav class='main'><ul><li class='active'>Home</a></li><li><a href='/firehose'>New</a></li></li>
+<li><form action='/search' method='post'><input type='search' name='search' placeholder='search' /></form></li></ul></nav>
 
 <?php if ( $thread->hasPosts() ) { ?>
 <h1>This just in</h1>
@@ -24,16 +25,14 @@ HTML;
 </ul>
 <?php } else { ?>
 <p>No posts found</p>
-<?php } 
-if ( CANPOST ) { ?>
+<?php } ?>
 <form method='post' action='/'>
 	<legend>New topic</legend>
 	<p><label for='title'>Title</label><input name='title' id='title' type='text' maxlength='60' required /></p>
 	<p><label for='body'>Message <span><a href='formatting.html' rel='tooltip' title='Simple HTML allowed'>formatting help</a></span></label><textarea name='body' id='body' rows='5' cols='60' required></textarea></p>
 	<p><input type='submit' class='button' value='Post'/></p>
 </form>
-<?php }
-\BB\Display::printJS(); ?>
+<?php \BB\Display::printJS(); ?>
 
 <script type='text/javascript' src='m.js'></script>
 </body>
