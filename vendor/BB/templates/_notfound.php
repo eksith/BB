9374e9b0b20bb8243d8d81023284d3f0360d78fa
Eksith Rodrigo <eksith@live.com>
2014-12-25 07:41:56 -0500

    Create _notfound.php

---
+++ vendor/BB/templates/_notfound.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" dir='ltr'>
+<head>
+<meta http-equiv='Content-Type' content='text/html; charset=utf-8' />
+<meta name='viewport' content='width=device-width' />
+<title>Not found</title>
+<link rel='stylesheet' href='/assets/s.css' />
+<link rel='shortcut icon' type='image/png' href='assets/img/favicon.png' />
+</head>
+<body>
+<h1>Not found</h1>
+<p>The page or post you're looking for is not found. <a href='/'>Back to index</a>.</p>
+</body>
+</html>
