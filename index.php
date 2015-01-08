e359c6d3c4cf96d5a8f62248adeb540f8c7896e6
Eksith Rodrigo <eksith@live.com>
2015-01-08 18:01:51 -0500

    Update index.php

--- index.php
+++ index.php
@@ -5,14 +5,11 @@ date_default_timezone_set( 'UTC' );
 
 // Performance testing. Remove in production (remember to also remove references below)
 define( 'START',	-microtime( true ) ); // Used with + microtime(true)
-$initialMemory		= memory_get_usage();
-
-session_start();
-
+$initialMemory	= memory_get_usage();
 
 /**
  * Application base path.
  */
 define( 'PATH',		realpath( dirname( __FILE__ ) ) . '/' );
 
-require( PATH . 'vendor/bootstrap.php' );
+require( PATH . 'vendor/BB/bootstrap.php' );
